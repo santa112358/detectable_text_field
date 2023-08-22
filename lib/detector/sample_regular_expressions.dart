5277bd56111aea747f609b107e9591da5e1a2670
furukaze_akane <furukaze.akane@gmail.com>
2023-08-23 00:47:40 +0900

    Fixed to recognize domains with only one character

--- lib/detector/sample_regular_expressions.dart
+++ lib/detector/sample_regular_expressions.dart
@@ -29,7 +29,7 @@ const detectionContentLetters = _symbols +
 
 const urlRegexContent = "((http|https)://)?(www.)?" +
     "[-a-zA-Z0-9@:%._\\+~#?&//=]" +
-    "{2,256}\\.[a-z]" +
+    "{1,256}\\.[a-z]" +
     "{2,6}\\b([-a-zA-Z0-9@:%" +
     "._\\+~#?&//=]*)";
 
