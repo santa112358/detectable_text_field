9d80fabb587c4990e9a2719dc51475f408f75618
vanvi <vanvi.dev@gmail.com>
2023-09-25 11:04:50 +0700

    Add DetectableTextFormField

--- lib/detectable_text_field.dart
+++ lib/detectable_text_field.dart
@@ -3,3 +3,4 @@ library detectable_text_field;
 export 'package:detectable_text_field/detectable_text_field.dart';
 
 export 'widgets/detectable_text.dart';
+export 'widgets/detectable_text_form_field.dart';
---
+++ lib/widgets/detectable_text_form_field.dart
@@ -0,0 +1,299 @@
+// Copyright 2014 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'dart:ui' as ui show BoxHeightStyle, BoxWidthStyle;
+
+import 'package:detectable_text_field/widgets/detectable_text_field.dart'
+    as dtf;
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+export 'package:flutter/services.dart'
+    show
+        TextInputType,
+        TextInputAction,
+        TextCapitalization,
+        SmartQuotesType,
+        SmartDashesType;
+
+class DetectableTextFormField extends FormField<String> {
+  DetectableTextFormField({
+    Key? key,
+    required RegExp detectionRegExp,
+    VoidCallback? onDetectionFinished,
+    ValueChanged<String>? onDetectionTyped,
+    String? initialValue,
+    this.controller,
+    TextStyle? basicStyle,
+    TextStyle? decoratedStyle,
+    FocusNode? focusNode,
+    InputDecoration? decoration = const InputDecoration(),
+    TextInputType? keyboardType,
+    TextInputAction? textInputAction,
+    TextCapitalization textCapitalization = TextCapitalization.none,
+    StrutStyle? strutStyle,
+    TextAlign textAlign = TextAlign.start,
+    TextAlignVertical? textAlignVertical,
+    TextDirection? textDirection,
+    bool readOnly = false,
+    ToolbarOptions? toolbarOptions,
+    bool? showCursor,
+    bool autofocus = false,
+    String obscuringCharacter = '•',
+    bool obscureText = false,
+    bool autocorrect = true,
+    SmartDashesType? smartDashesType,
+    SmartQuotesType? smartQuotesType,
+    bool enableSuggestions = true,
+    int? maxLines = 1,
+    int? minLines,
+    bool expands = false,
+    int? maxLength,
+    @Deprecated(
+        'Use maxLengthEnforcement parameter which provides more specific '
+        'behavior related to the maxLength limit. '
+        'This feature was deprecated after v1.25.0-5.0.pre.')
+    bool maxLengthEnforced = true,
+    MaxLengthEnforcement? maxLengthEnforcement,
+    ValueChanged<String>? onChanged,
+    VoidCallback? onEditingComplete,
+    ValueChanged<String>? onSubmitted,
+    AppPrivateCommandCallback? onAppPrivateCommand,
+    super.onSaved,
+    super.validator,
+    List<TextInputFormatter>? inputFormatters,
+    bool? enabled,
+    double cursorWidth = 2.0,
+    double? cursorHeight,
+    Radius? cursorRadius,
+    Color? cursorColor,
+    ui.BoxHeightStyle selectionHeightStyle = ui.BoxHeightStyle.tight,
+    ui.BoxWidthStyle selectionWidthStyle = ui.BoxWidthStyle.tight,
+    Brightness? keyboardAppearance,
+    EdgeInsets scrollPadding = const EdgeInsets.all(20.0),
+    DragStartBehavior dragStartBehavior = DragStartBehavior.start,
+    bool enableInteractiveSelection = true,
+    TextSelectionControls? selectionControls,
+    GestureTapCallback? onTap,
+    MouseCursor? mouseCursor,
+    dtf.InputCounterWidgetBuilder? buildCounter,
+    AutovalidateMode? autovalidateMode,
+    ScrollController? scrollController,
+    ScrollPhysics? scrollPhysics,
+    Iterable<String>? autofillHints,
+    String? restorationId,
+    SpellCheckConfiguration? spellCheckConfiguration,
+  })  : assert(initialValue == null || controller == null),
+        assert(obscuringCharacter.length == 1),
+        assert(maxLines == null || maxLines > 0),
+        assert(minLines == null || minLines > 0),
+        assert(
+          (maxLines == null) || (minLines == null) || (maxLines >= minLines),
+          "minLines can't be greater than maxLines",
+        ),
+        assert(
+          !expands || (maxLines == null && minLines == null),
+          'minLines and maxLines must be null when expands is true.',
+        ),
+        assert(!obscureText || maxLines == 1,
+            'Obscured fields cannot be multiline.'),
+        assert(maxLength == null ||
+            maxLength == dtf.DetectableTextField.noMaxLength ||
+            maxLength > 0),
+        super(
+          initialValue:
+              controller != null ? controller.text : (initialValue ?? ''),
+          enabled: enabled ?? decoration?.enabled ?? true,
+          autovalidateMode: autovalidateMode ?? AutovalidateMode.disabled,
+          builder: (FormFieldState<String> field) {
+
+            final _DetectableTextFormFieldState state = field as _DetectableTextFormFieldState;
+            final InputDecoration effectiveDecoration = (decoration ?? const InputDecoration())
+                .applyDefaults(Theme.of(field.context).inputDecorationTheme);
+
+            void onChangedHandler(String value) {
+              field.didChange(value);
+              if (onChanged != null) {
+                onChanged(value);
+              }
+            }
+
+            return dtf.DetectableTextField(
+              detectionRegExp: detectionRegExp,
+              controller: state._effectiveController,
+              basicStyle: basicStyle,
+              decoratedStyle: decoratedStyle,
+              onDetectionFinished: onDetectionFinished,
+              onDetectionTyped: onDetectionTyped,
+              focusNode: focusNode,
+              decoration: effectiveDecoration.copyWith(errorText: field.errorText),
+              keyboardType: keyboardType,
+              textInputAction: textInputAction,
+              textCapitalization: textCapitalization,
+              strutStyle: strutStyle,
+              textAlign: textAlign,
+              textAlignVertical: textAlignVertical,
+              textDirection: textDirection,
+              readOnly: readOnly,
+              toolbarOptions: toolbarOptions,
+              showCursor: showCursor,
+              autofocus: autofocus,
+              obscuringCharacter: obscuringCharacter,
+              obscureText: obscureText,
+              autocorrect: autocorrect,
+              smartDashesType: smartDashesType ?? (obscureText ? SmartDashesType.disabled : SmartDashesType.enabled),
+              smartQuotesType: smartQuotesType ?? (obscureText ? SmartQuotesType.disabled : SmartQuotesType.enabled),
+              enableSuggestions: enableSuggestions,
+              maxLines: maxLines,
+              minLines: minLines,
+              expands: expands,
+              maxLength: maxLength,
+              maxLengthEnforced: maxLengthEnforced,
+              maxLengthEnforcement: maxLengthEnforcement,
+              onChanged: onChangedHandler,
+              onEditingComplete: onEditingComplete,
+              onSubmitted: onSubmitted,
+              onAppPrivateCommand: onAppPrivateCommand,
+              inputFormatters: inputFormatters,
+              enabled: enabled,
+              cursorWidth: cursorWidth,
+              cursorHeight: cursorHeight,
+              cursorRadius: cursorRadius,
+              cursorColor: cursorColor,
+              selectionHeightStyle: selectionHeightStyle,
+              selectionWidthStyle: selectionWidthStyle,
+              keyboardAppearance: keyboardAppearance,
+              scrollPadding: scrollPadding,
+              dragStartBehavior: dragStartBehavior,
+              enableInteractiveSelection: enableInteractiveSelection,
+              selectionControls: selectionControls,
+              onTap: onTap,
+              mouseCursor: mouseCursor,
+              buildCounter: buildCounter,
+              scrollController: scrollController,
+              scrollPhysics: scrollPhysics,
+              autofillHints: autofillHints,
+              restorationId: restorationId,
+              spellCheckConfiguration: spellCheckConfiguration,
+            );
+          },
+        );
+
+  /// Controls the text being edited.
+  ///
+  /// If null, this widget will create its own [TextEditingController] and
+  /// initialize its [TextEditingController.text] with [initialValue].
+  final TextEditingController? controller;
+
+  @override
+  FormFieldState<String> createState() => _DetectableTextFormFieldState();
+}
+
+class _DetectableTextFormFieldState extends FormFieldState<String> {
+  RestorableTextEditingController? _controller;
+
+  TextEditingController get _effectiveController =>
+      _textFormField.controller ?? _controller!.value;
+
+  DetectableTextFormField get _textFormField =>
+      super.widget as DetectableTextFormField;
+
+  @override
+  void restoreState(RestorationBucket? oldBucket, bool initialRestore) {
+    super.restoreState(oldBucket, initialRestore);
+    if (_controller != null) {
+      _registerController();
+    }
+    // Make sure to update the internal [FormFieldState] value to sync up with
+    // text editing controller value.
+    setValue(_effectiveController.text);
+  }
+
+  void _registerController() {
+    assert(_controller != null);
+    registerForRestoration(_controller!, 'controller');
+  }
+
+  void _createLocalController([TextEditingValue? value]) {
+    assert(_controller == null);
+    _controller = value == null
+        ? RestorableTextEditingController()
+        : RestorableTextEditingController.fromValue(value);
+    if (!restorePending) {
+      _registerController();
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    if (_textFormField.controller == null) {
+      _createLocalController(widget.initialValue != null
+          ? TextEditingValue(text: widget.initialValue!)
+          : null);
+    } else {
+      _textFormField.controller!.addListener(_handleControllerChanged);
+    }
+  }
+
+  @override
+  void didUpdateWidget(DetectableTextFormField oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (_textFormField.controller != oldWidget.controller) {
+      oldWidget.controller?.removeListener(_handleControllerChanged);
+      _textFormField.controller?.addListener(_handleControllerChanged);
+
+      if (oldWidget.controller != null && _textFormField.controller == null) {
+        _createLocalController(oldWidget.controller!.value);
+      }
+
+      if (_textFormField.controller != null) {
+        setValue(_textFormField.controller!.text);
+        if (oldWidget.controller == null) {
+          unregisterFromRestoration(_controller!);
+          _controller!.dispose();
+          _controller = null;
+        }
+      }
+    }
+  }
+
+  @override
+  void dispose() {
+    _textFormField.controller?.removeListener(_handleControllerChanged);
+    _controller?.dispose();
+    super.dispose();
+  }
+
+  @override
+  void didChange(String? value) {
+    super.didChange(value);
+
+    if (_effectiveController.text != value) {
+      _effectiveController.text = value ?? '';
+    }
+  }
+
+  @override
+  void reset() {
+    // setState will be called in the superclass, so even though state is being
+    // manipulated, no setState call is needed here.
+    _effectiveController.text = widget.initialValue ?? '';
+    super.reset();
+  }
+
+  void _handleControllerChanged() {
+    // Suppress changes that originated from within this class.
+    //
+    // In the case where a controller has been passed in to this widget, we
+    // register this change listener. In these cases, we'll also receive change
+    // notifications for changes originating from within this class -- for
+    // example, the reset() method. In such cases, the FormField value will
+    // already have been set.
+    if (_effectiveController.text != value) {
+      didChange(_effectiveController.text);
+    }
+  }
+}
