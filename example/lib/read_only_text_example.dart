01448ae10781e0d0966f1f93980355c26da47760
santa112358 <sun.jp.0212@gmail.com>
2023-12-02 14:59:01 +0900

    Fix example structure

---
+++ example/lib/read_only_text_example.dart
@@ -0,0 +1,48 @@
+import 'package:detectable_text_field/detector/sample_regular_expressions.dart';
+import 'package:detectable_text_field/widgets/detectable_text.dart';
+import 'package:flutter/material.dart';
+
+class ReadOnlyText extends StatelessWidget {
+  const ReadOnlyText({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return DetectableText(
+      trimLines: 1,
+      colorClickableText: Colors.pink,
+      trimMode: TrimMode.Line,
+      trimCollapsedText: 'more',
+      trimExpandedText: '...less',
+      text:
+          "Welcome to #Detectable @TextField http://www.google.com this is sample text we are texting the feed text here. Welcome to #Detectable @TextField this is sample text we are texting the feed text here",
+      detectionRegExp: RegExp(
+        "(?!\\n)(?:^|\\s)([#@]([$detectionContentLetters]+))|$urlRegexContent",
+        multiLine: true,
+      ),
+      onExpansionChanged: (bool readMore) {
+        debugPrint('Read more >>>>>>> $readMore');
+      },
+      onTap: (tappedText) async {
+        debugPrint(tappedText);
+        if (tappedText.startsWith('#')) {
+          debugPrint('DetectableText >>>>>>> #');
+        } else if (tappedText.startsWith('@')) {
+          debugPrint('DetectableText >>>>>>> @');
+        } else if (tappedText.startsWith('http')) {
+          debugPrint('DetectableText >>>>>>> http');
+        }
+      },
+      basicStyle: const TextStyle(
+        fontSize: 16,
+        fontWeight: FontWeight.w400,
+        color: Colors.grey,
+      ),
+      detectedStyle: const TextStyle(
+        fontSize: 16,
+        fontWeight: FontWeight.w500,
+        letterSpacing: 0.5,
+        color: Colors.blueAccent,
+      ),
+    );
+  }
+}
